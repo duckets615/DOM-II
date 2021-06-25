0c9af83f4a8273c82f84beb44acfd225d996b491
ChrisReilly024 <chris.reilly024@gmail.com>
2021-06-25 09:32:34 -0400

    greensock installed, spin eventlistener action on top pic

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+import { gsap } from "gsap";
+
+
+const topPic = document.querySelector('.top-pic')
+console.log(topPic)
+
+topPic.addEventListener('dblclick', () => gsap.to(".top-pic", {duration: 3, rotation: 360, scale: 1}))
