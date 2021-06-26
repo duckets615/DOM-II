5da41cb11574d8518d95f8042e5e8782d1d7129d
ChrisReilly024 <chris.reilly024@gmail.com>
2021-06-26 03:21:06 -0400

    font color change click event added

--- js/index.js
+++ js/index.js
@@ -2,11 +2,14 @@ import { gsap } from "gsap";
 // targets
 const topPic = document.querySelector('.top-pic');
 const busHeader = document.querySelector('.busHeader');
+const topPara = document.querySelector('.top-para')
 
 // animation functions
 const spin = (e) => gsap.to(e.target, {duration: 0.5, rotation: 720}).restart(false,false);
 const center = (e) => gsap.to(e.target, {duration: 1, x: 275});
-
+const colorChange = (e) => e.target.classList.toggle('font-change')
 // execution
 topPic.addEventListener('dblclick', spin);
 busHeader.addEventListener('click', center);
+topPara.addEventListener('click', colorChange);
+
